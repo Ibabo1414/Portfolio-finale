66277c6d2e1bd0a7864e55b087d63c5fa5ece712
Ibabo <iyourimendoza@gmail.com>
2020-06-12 18:45:50 +0100

    Ajout du Chargeur

---
+++ js/loader.js
@@ -0,0 +1,7 @@
+/**
+ *class "Chargeur (loader) hidden"
+ */
+window.addEventListener("load", function () {
+    const tout = document.querySelector(".tout");
+    tout.className += " hidden"; 
+});
