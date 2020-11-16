dc61c3271a3ccea8d126b9ebcf121a72b848d79f
ymii89 <francoisym3@gmail.com>
2020-11-16 08:55:39 +0100

    Modification du compteur

--- js/gs-sheet.js
+++ js/gs-sheet.js
@@ -2,8 +2,8 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbwhHb85DlY2kvnDc-v4DF
 const form = document.forms['google-sheet']
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Félication, votre requêtte à bien été enregistrer ! Vous pouvez nous contacter au 077 76 25 56"))
-    .catch(error => console.error('Error!', error.message))
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => alert("Félication, votre requêtte à bien été enregistrer ! Vous pouvez nous contacter au 077 76 25 56"))
+        .catch(error => console.error('Error!', error.message))
 })
